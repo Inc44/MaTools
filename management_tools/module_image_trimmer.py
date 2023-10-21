ebbae4bb1457caacf90a921b4872193e0da69978
Inc <121856799+Inc44@users.noreply.github.com>
2023-10-21 02:04:30 +0200

    Rust Trim Engine

--- management_tools/module_image_trimmer.py
+++ management_tools/module_image_trimmer.py
@@ -1,75 +1,38 @@
 import os
 from datetime import datetime
 from pathlib import Path
-from PIL import ImageGrab, Image
+from tempfile import NamedTemporaryFile
+from PIL import ImageGrab
+import rusty_scissors_pyo3
 
 
-def save_image(trimmed_image):
+def generate_file_name() -> str:
     desktop_path = Path(os.path.join(os.environ["USERPROFILE"], "Desktop"))
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = desktop_path / f"screentrim_{timestamp}.png"
-
-    if trimmed_image.size[0] > 0 and trimmed_image.size[1] > 0:
-        trimmed_image.save(file_path, "PNG")
+    return str(desktop_path / f"screentrim_{timestamp}.png")
 
 
-def trim_image(clipboard_image):
-    bg_color = clipboard_image.getpixel((0, 0))
-    bounding_box = clipboard_image.getbbox()
-
-    trimmed_image = clipboard_image.crop(bounding_box)
-
-    while all(
-        trimmed_image.getpixel((0, i)) == bg_color for i in range(trimmed_image.size[1])
-    ):
-        trimmed_image = trimmed_image.crop(
-            (1, 0, trimmed_image.size[0], trimmed_image.size[1])
-        )
-
-    while all(
-        trimmed_image.getpixel((i, 0)) == bg_color for i in range(trimmed_image.size[0])
-    ):
-        trimmed_image = trimmed_image.crop(
-            (0, 1, trimmed_image.size[0], trimmed_image.size[1])
-        )
-
-    while all(
-        trimmed_image.getpixel((trimmed_image.size[0] - 1, i)) == bg_color
-        for i in range(trimmed_image.size[1])
-    ):
-        trimmed_image = trimmed_image.crop(
-            (0, 0, trimmed_image.size[0] - 1, trimmed_image.size[1])
-        )
-
-    while all(
-        trimmed_image.getpixel((i, trimmed_image.size[1] - 1)) == bg_color
-        for i in range(trimmed_image.size[0])
-    ):
-        trimmed_image = trimmed_image.crop(
-            (0, 0, trimmed_image.size[0], trimmed_image.size[1] - 1)
-        )
-
-    return trimmed_image
+def save_image(trimmed_image_path: str, save_path: str) -> None:
+    os.rename(trimmed_image_path, save_path)
 
 
-def clipboard_image_trimmer():
+def clipboard_image_trimmer(override: bool = True) -> None:
     clipboard_image = ImageGrab.grabclipboard()
-    if clipboard_image:
-        trimmed_image = trim_image(clipboard_image)
-        if trimmed_image:
-            save_image(trimmed_image)
+    if clipboard_image is not None:
+        with NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
+            clipboard_image.save(temp_file.name, "PNG")
+            temp_file.close()
+            rusty_scissors_pyo3.process_image_py(temp_file.name, override)
+            save_image(temp_file.name, generate_file_name())
 
 
-def path_image_trimmer(image_path):
-    image = Image.open(image_path)
-    trimmed_image = trim_image(image)
-    if trimmed_image:
-        file_name = os.path.basename(image_path)
-        save_dir = os.path.dirname(image_path)
-        save_path = os.path.join(save_dir, f"trimmed_{file_name}")
-        trimmed_image.save(save_path)
-
-
-def path_images_trimmer(image_paths_to_trim):
+def path_images_trimmer(image_paths_to_trim: list[str], override: bool = False) -> None:
     for image_path_to_trim in image_paths_to_trim:
-        path_image_trimmer(image_path_to_trim)
+        if os.path.isfile(image_path_to_trim):
+            rusty_scissors_pyo3.process_image_py(image_path_to_trim, override)
+        # elif os.path.isdir(image_paths_to_trim):
+        #    rusty_scissors_pyo3.process_directory_py(image_paths_to_trim, override)
+
+
+if __name__ == "__main__":
+    clipboard_image_trimmer()
---
+++ management_tools/rusty_scissors_pyo3/src/lib.rs
@@ -0,0 +1,22 @@
+use pyo3::prelude::*;
+use rusty_scissors::{process_directory, process_image};
+use std::path::Path;
+
+#[pymodule]
+fn rusty_scissors_pyo3(_py: Python, m: &PyModule) -> PyResult<()> {
+    #[pyfn(m, name = "process_image_py")]
+    fn process_image_py(_py: Python, input_path: &str, override_flag: bool) -> PyResult<()> {
+        let path = Path::new(input_path);
+        process_image(&path, override_flag)
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))
+    }
+
+    #[pyfn(m, name = "process_directory_py")]
+    fn process_directory_py(_py: Python, input_path: &str, override_flag: bool) -> PyResult<()> {
+        let path = Path::new(input_path);
+        process_directory(&path, override_flag)
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyException, _>(format!("{}", e)))
+    }
+
+    Ok(())
+}
