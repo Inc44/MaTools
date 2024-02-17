e8ff8449c2319f606fe254cdc632bdc6c49e3b34
Inc44 <incognita44@outlook.com>
2024-02-18 00:38:45 +0100

    Add jpg

--- management_tools/panel_png_to_pdf_converter.py
+++ management_tools/panel_png_to_pdf_converter.py
@@ -12,7 +12,7 @@ class PngToPdfConverterPanel(QWidget):
         super().__init__()
         layout = QVBoxLayout()
 
-        self.list_widget = DraggableListWidget(accepted_file_extensions=[".png"])
+        self.list_widget = DraggableListWidget(accepted_file_extensions=[".png", ".jpg"])
 
         self.convert_button = QPushButton("Convert")
         self.convert_button.clicked.connect(self.convert_pngs_to_pdf)
